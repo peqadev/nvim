7ad78f15111797a6460370828283e2c792d80b56
Erick Agrazal <eagrazal@thumbstopper.com>
2022-12-13 17:12:16 -0500

    Added ctrl up and down keymaps.

--- lua/erick/core/keymaps.lua
+++ lua/erick/core/keymaps.lua
@@ -8,12 +8,14 @@ keymap.set("i", "jk", "<ESC>")
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 keymap.set("n", "x", '"_x')
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "<C-d>", "<C-d>zz")
 
 keymap.set("n", "<leader>+", "<C-a>")
 keymap.set("n", "<leader>-", "<C-x>")
 
 keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally 
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width/height
 keymap.set("n", "<leader>sx", ":close<CR>") -- Close current split window
 
