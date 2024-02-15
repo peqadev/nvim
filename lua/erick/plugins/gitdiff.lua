7d8c833e64679baeaa129b8ac1a5c3be9535ebf2
Erick Agrazal <eagrazal@thumbstopper.com>
2024-02-15 03:07:40 +0000

    Added Diffview keymaps.

--- lua/erick/plugins/gitdiff.lua
+++ lua/erick/plugins/gitdiff.lua
@@ -1,3 +1,8 @@
 return {
   "sindrets/diffview.nvim",
+  config = function()
+    vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<CR>", { desc = "Open Diff View" }) -- open diffview open
+    vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<CR>", { desc = "Close Diff View" }) -- close diffview open
+    vim.keymap.set("n", "<leader>df", "<cmd>DiffviewToggleFiles<CR>", { desc = "Toggle Diff View Files" }) -- toggle diffview files
+  end,
 }
