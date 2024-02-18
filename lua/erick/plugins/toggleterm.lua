f66dfe5d793ac3cc0781c7d9673834b0afe9ef3f
Erick Vicente Agrazal Lopez <erick@agrazal.com>
2024-02-17 19:17:33 -0500

    dded toggleterm.

---
+++ lua/erick/plugins/toggleterm.lua
@@ -0,0 +1,25 @@
+function _G.set_terminal_keymaps()
+  local opts = { noremap = true }
+  vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+end
+
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
+return {
+  "akinsho/toggleterm.nvim",
+  opts = {
+    open_mapping = [[<c-1>]],
+    direction = "float",
+    shade_filetypes = {},
+    hide_numbers = true,
+    insert_mappings = true,
+    terminal_mappings = true,
+    start_in_insert = true,
+    close_on_exit = true,
+  },
+}
